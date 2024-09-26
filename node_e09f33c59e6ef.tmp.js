c8ea9e0eb3b149e03a55be16b9b5d675a1bc916d
ErivaldoChaves <erivas_12@hotmail.com>
2024-09-26 11:12:10 -0300

    jogo da velha

--- node_e09f33c59e6ef.tmp.js
+++ node_e09f33c59e6ef.tmp.js
@@ -42,6 +42,19 @@ while (!ganhou){
     }
 }*/
 
+var readline = require('readline');
+var resp = "";
+
+var leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("Qual módulo pra ler dados no node.js?\n", function(answer) {
+    var resp = answer;
+    console.log("\nSua resposta '" + resp + "' foi grava com sucesso numa variável inútil");
+    leitor.close();
+});
 
 
 
